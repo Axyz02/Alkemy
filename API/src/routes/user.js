d6c42862272b81c092b62bd8f43b72e23ce65b27
Axyz02 <nicolasig002@gmail.com>
2021-02-24 00:07:09 -0300

    fixed concept delete to ID delete

--- API/src/routes/user.js
+++ API/src/routes/user.js
@@ -41,22 +41,23 @@ router.post('/',(req,res) =>{
 })
 
 // TODO PUT
-/*router.put('/:concept',(req,res) =>{
-    const {id, concept, amount, date, type} = req.body;
-    mysqlConnection.query(`INSERT INTO user VALUES(${id},'${concept}', ${amount},'${date}','${type}');`,(err,rows,fields) =>{
+router.put('/:id',(req,res) =>{
+    const {id,concept, amount, date, type} = req.body;
+    mysqlConnection.query(`UPDATE user SET concept = '${concept}', SET amount = ${amount}, SET date = '${date}', SET type = '${type}' WHERE id = ${id});`,(err,rows,fields) =>{
         if(!err){
-            res.json({Status: 'Movement Saved'});
+            res.json({Status: 'Movement Updated'});
         } else {
             console.log(err);
         }
     })
-}) */
+})
+
 // TODO DELETE
-router.delete('/:concept', (req,res) =>{
-    const { concept } = req.params;
-    mysqlConnection.query(`DELETE FROM user WHERE concept = "${concept}"`, (err,rows,fields) =>{
+router.delete('/:id', (req,res) =>{
+    const { id } = req.params;
+    mysqlConnection.query(`DELETE FROM user WHERE id = "${id}"`, (err,rows,fields) =>{
         if(!err){
-            res.json(rows[0]);
+            res.json({Status: 'Movement Deleted'});
         } else{
             console.log(err);
         }
